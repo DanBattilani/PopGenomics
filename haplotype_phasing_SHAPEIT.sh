0606afce80563f03c737a660bb57744e3320f7a2
DanBattilani <136367679+DanBattilani@users.noreply.github.com>
2025-06-06 15:10:15 +0200

    Generalize scripts with configurable variables

--- haplotype_phasing_SHAPEIT.sh
+++ haplotype_phasing_SHAPEIT.sh
@@ -1,49 +1,56 @@
+#!/bin/bash
 # MAIN PRINCIPLE: the larger the dataset to phase, the higher the phasing quality.
 
+# Input settings
+plink_prefix="file"
+map_file="genetic_map.txt"
+ids_file="file.subset.inds"
+out_prefix="file.phased"
+
 # Haplotype phasing starting from bed/bim/fam (binary PLINK files) using SHAPEIT2.
-shapeit --input-bed file.bed file.bim file.fam \
---input-map genetic_map.txt \
---output-haps file.phased.haps file.phased.sample
+shapeit --input-bed "${plink_prefix}.bed" "${plink_prefix}.bim" "${plink_prefix}.fam" \
+    --input-map "$map_file" \
+    --output-haps "${out_prefix}.haps" "${out_prefix}.sample"
 
 
 #-------------------------------------------------------------------------------------------
 # (if needed) exclude/include individuals to only phase a particular set of samples (.inds -> one column with individual IDs)
 # EXCLUDE
-shapeit --input-bed file \
---input-map genetic_map.txt \
---exclude-ind file.subset.inds \
---output-haps file.phased
+shapeit --input-bed "${plink_prefix}.bed" "${plink_prefix}.bim" "${plink_prefix}.fam" \
+    --input-map "$map_file" \
+    --exclude-ind "$ids_file" \
+    --output-haps "${out_prefix}.exclude"
 
 # INCLUDE
-shapeit --input-bed file \
---input-map genetic_map.txt \
---include-ind file.subset.inds \
---output-haps file.phased
+shapeit --input-bed "${plink_prefix}.bed" "${plink_prefix}.bim" "${plink_prefix}.fam" \
+    --input-map "$map_file" \
+    --include-ind "$ids_file" \
+    --output-haps "${out_prefix}.include"
 
 
 #-------------------------------------------------------------------------------------------
 # SHAPEIT generates .haps file which you may want to convert to other formats.
 shapeit --convert \
---input-haps file.phased \
---output-ref file.phased.hap file.phased.leg file.phased.sam
+    --input-haps "$out_prefix" \
+    --output-ref "${out_prefix}.hap" "${out_prefix}.leg" "${out_prefix}.sam"
 
 # You can also convert to VCF
 shapeit --convert \
---input-haps file.phased \
---output-vcf file.phased.vcf
+    --input-haps "$out_prefix" \
+    --output-vcf "${out_prefix}.vcf"
 
 
 #-------------------------------------------------------------------------------------------
 # You can subset the phased dataset.
 # EXCLUDE
 shapeit --convert \
---input-haps file.phased \
---exclude-ind file.subset.inds \
---output-haps subset.phased
+    --input-haps "$out_prefix" \
+    --exclude-ind "$ids_file" \
+    --output-haps "${out_prefix}.subset"
 
 # INCLUDE
 shapeit --convert \
---input-haps file.phased \
---include-ind file.subset.inds \
---output-haps subset.phased
+    --input-haps "$out_prefix" \
+    --include-ind "$ids_file" \
+    --output-haps "${out_prefix}.subset"
 
